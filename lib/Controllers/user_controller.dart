c7342f5bd18ffe54ab47b32d77578d4ae0c05dad
MohammadAftabAlam <mohammadaftab1062@gmail.com>
2025-05-31 23:52:19 +0530

    controllers added

---
+++ lib/Controllers/user_controller.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class UserController{
+  final String baseUrl = "http://localhost:3000";
+
+  Future<http.Response> getRequest(String endPoint) async{
+    final uri = Uri.parse("$baseUrl/$endPoint");
+    try{
+      final response = await http.get(uri);
+      return response;
+    }catch(err){
+      return throw Exception("GET request error $err");
+    }
+  }
+
+  Future<http.Response> postRequest(String endpoint, Map<String, dynamic> data) async {
+    final url = Uri.parse('$baseUrl/$endpoint');
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode(data),
+      );
+      return response;
+    } catch (e) {
+      throw Exception('POST request error: $e');
+    }
+  }
+}
